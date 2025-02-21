6c2869d65540da782427ba1f5d7713171af0d3e7
Yuvi G <guptayuvi636@gmail.com>
2025-02-21 09:37:15 -0700

    Add files via upload

---
+++ Website/streamlit_app.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+import json
+import streamlit as st
+from streamlit_lottie import st_lottie
+from streamlit_extras.let_it_rain import rain
+
+# Directories and file paths
+THIS_DIR = Path(__file__).parent
+CSS_FILE = THIS_DIR / "style" / "style.css"
+ASSETS = THIS_DIR / "assets"
+LOTTIE_ANIMATION = ASSETS / "Testing.json"
+
+
+# Function to load and display the Lottie animation
+def load_lottie_animation(file_path):
+    with open(file_path, "r") as f:
+        return json.load(f)
+
+
+# Function to apply snowfall effect
+def run_snow_animation():
+    rain(emoji="💀", font_size=20, falling_speed=5, animation_length="infinite")
+
+
+# Page configuration
+st.set_page_config(page_title="TEST DEMO ONLY", page_icon="🧪")
+
+# Run snowfall animation
+run_snow_animation()
+
+# Apply custom CSS
+with open(CSS_FILE) as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+# Display header with personalized name
+
+st.header(f"Development Model", anchor=False)
+
+# Display the Lottie animation
+lottie_animation = load_lottie_animation(LOTTIE_ANIMATION)
+st_lottie(lottie_animation, key="lottie-holiday", height=300)
+
+# Personalized holiday message
+st.markdown(
+    f"This is a test. Updates will come in the future"\
+)
+
